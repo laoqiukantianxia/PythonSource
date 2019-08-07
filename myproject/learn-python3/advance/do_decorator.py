66b4a2149c4007ce67e493a954d3bec0b766de02
qiushichao <qiushichao66@163.com>
2019-08-07 08:31:21 +0800

    add decorator

--- myproject/learn-python3/advance/do_decorator.py
+++ myproject/learn-python3/advance/do_decorator.py
@@ -1,19 +1,47 @@
+# -*- coding:utf-8 -*-
+
+
+"""
+这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
+本质上，decorator就是一个返回函数的高阶函数。
+"""
+
+
 import functools
 import time
 
 
-def log1(func):
-	@functools.wraps(func) # 保持属性不变
+def log(func):
+	#@functools.wraps(func)  # 保持属性不变
 	def wrapper(*args, **kw):
 		print("call %s():" % func.__name__)
 		return func(*args, **kw)
 	return wrapper
 
-@log1
+
+@log
 def now():
-	print("2019")
+	print("hello now")
+
+
+def log1(test):
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			print("%s:call %s():" % (test, func.__name__))
+			return func(*args, **kw)
+		return wrapper
+	return decorator
+
+
+@log1(time.strftime('%Y-%m-%d %H:%M:%S'))
+def now1():
+	print("hello now1")
+
 
 
 if __name__ == "__main__":
 	now()
-	print(now.__name__)
+	print(now.__name__)  # now.__name__ = wrapper
+	now1()
+	print(now1.__name__) # now1.__name__ = now1
--- myproject/learn-python3/basic/time.py
+++ myproject/learn-python3/basic/time.py
@@ -5,6 +5,8 @@ import datetime
 
 
 print(time.time())
+print(time.localtime())
+print(time.localtime(time.time()))
 
 # 格式化时间
 print(time.strftime('%Y-%m-%d %H:%M:%S'))
