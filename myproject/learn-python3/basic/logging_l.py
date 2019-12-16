4b9a99572cff9fbdb58332c702210ae1c3339162
qiushichao <qiushichao66@163.com>
2019-12-16 08:46:20 +0800

    webssh

--- myproject/learn-python3/basic/logging_l.py
+++ myproject/learn-python3/basic/logging_l.py
@@ -2,6 +2,20 @@
 
 import logging
 
+    """
+    logging高级用法：
+    1. 四大组件：
+        记录器    Logger    提供了应用程序可一直使用的接口
+        处理器    Handler   将logger创建的日志记录发送到合适的目的输出
+        过滤器    Filter    提供了更细粒度的控制工具来决定输出哪条日志记录，丢弃哪条日志记录
+        格式器    Formatter 决定日志记录的最终输出格式
+    
+    即：日志器（logger）是入口，真正干活儿的是处理器（handler），
+    （handler）还可以通过过滤器（filter）和格式器（formatter）对要输出的日志内容做过滤和格式化等处理操作。
+"""
+
+
+
 '''
 # simple
 logging.warning('task')
