c2b36d128dd3ce650a129e4e414115a42877b3b5
qiushichao <984039865@qq.com>
2020-02-19 18:05:55 +0800

    update

--- myproject/learn-python3/basic/3_func.py
+++ myproject/learn-python3/basic/3_func.py
@@ -28,3 +28,49 @@ print(f1(2, 3))
 f2 = lambda: 'test'
 print(f2())
 
+# 2. 高阶函数
+# 把函数作为参数传入，这样的函数称为高阶函数，函数式编程就是指这种高度抽象的编程范式
+## a. 变量可以指向函数
+## b. 函数名也是变量
+## c. 传入函数
+f = abs
+print(-10, f, f(-10))
+abs = 10
+print(abs)
+
+def test(x, y, f):
+    return f(x) + f(y)
+print(test(-10, -10, f))
+
+## 2.1 map
+### map()函数接收两个参数，一个是函数，一个是Iterable，
+### map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回。
+
+l = [1, -9, 29, 10, -30, -29]
+print(list(map(f, l)))
+
+## 2.2 reduce
+### 把一个函数作用在一个序列[x1, x2, x3, ...]上，这个函数必须接收两个参数，
+### reduce把结果继续和序列的下一个元素做累积计算
+from functools import reduce
+def add(x,y):
+    return x+y
+l = [1, 2, 3, 4, 5]
+print(reduce(add, l))
+
+#### example：将[1, 3, 5, 7, 9] 变换成13579
+l = [1, 3, 5, 7, 9]
+def fn(x, y):
+    return x * 10 + y
+print(reduce(fn, l))
+
+#### example:str2int
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+def str2int(s):
+    def fn(x, y):
+        return x*10 + y
+    def char2num(s):
+        return DIGITS[s]
+    return reduce(fn, map(char2num, s))
+
+print(str2int('123456'))
